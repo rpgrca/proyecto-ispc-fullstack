414bdb7265a1b4f45f13dcce96d0f78edeacc7c2
Roberto <rpgrca@gmail.com>
2022-10-19 00:04:49 -0300

    implementar controlador login

--- controller/login.py
+++ controller/login.py
@@ -1,3 +1,19 @@
 # login.py
 #
-# login(usuario, password)
+from typing import Union
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+class LoginController:
+    def __init__(self, usuario: str, clave: str):
+        model
+        self.__usuario = usuario
+        self.__clave = clave
+        self.__response = { "status": "error" }
+
+        if usuario in Login.__usuarios:
+            if Login.__usuarios[usuario] == clave:
+                self.__response = { "status": "ok" }
+
+    def obtener_respuesta(self):
+        return self.__response
