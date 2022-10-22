44d7f6d9defd9708718d662e272ba4c425695b60
Roberto <rpgrca@gmail.com>
2022-10-22 00:15:18 -0300

    quitando main del archivo de testing

--- main_tests.py
+++ main_tests.py
@@ -3,11 +3,10 @@ from tests.login_tests import LoginControllerTests
 from tests.registro_tests import RegistroControllerTests
 from tests.reestablecer_tests import ReestablecerControllerTests
 
-if __name__ == "__main__":
-    suite = unittest.TestSuite()
-    
-    suite.addTest(unittest.makeSuite(LoginControllerTests))
-    suite.addTest(unittest.makeSuite(RegistroControllerTests))
-    suite.addTest(unittest.makeSuite(ReestablecerControllerTests))
-    
-    unittest.TextTestRunner(verbosity=3).run(suite)
+suite = unittest.TestSuite()
+
+suite.addTest(unittest.makeSuite(LoginControllerTests))
+suite.addTest(unittest.makeSuite(RegistroControllerTests))
+suite.addTest(unittest.makeSuite(ReestablecerControllerTests))
+
+unittest.TextTestRunner(verbosity=3).run(suite)
