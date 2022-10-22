f07eb4456e4af85d1591ef354b8d7d8a5a12d75c
Roberto <rpgrca@gmail.com>
2022-10-21 23:06:00 -0300

    agregar pruebas de registracion al suite

--- main_tests.py
+++ main_tests.py
@@ -1,9 +1,11 @@
 import unittest
 from tests.login_tests import LoginControllerTests
+from tests.registro_tests import RegistroControllerTests
 
 if __name__ == "__main__":
     suite = unittest.TestSuite()
     
     suite.addTest(unittest.makeSuite(LoginControllerTests))
+    suite.addTest(unittest.makeSuite(RegistroControllerTests))
     
     unittest.TextTestRunner(verbosity=3).run(suite)
