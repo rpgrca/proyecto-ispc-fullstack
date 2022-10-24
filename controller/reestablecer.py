14acf79daea6a6920fd31bc33b18aa30535d7807
Roberto <rpgrca@gmail.com>
2022-10-23 22:33:55 -0300

    extraer constantes del controlador de reestablecer

--- controller/reestablecer.py
+++ controller/reestablecer.py
@@ -3,9 +3,11 @@ from controller.email_sender import EmailSender, RealEmailSender
 from model.database import BaseDeDatos
 
 class ReestablecerController(Controller):
+    RECORDATORIO_EXITOSO = "Si el correo está en nuestros registros se ha enviado un recordatorio a su cuenta"
+
     def __init__(self, db: BaseDeDatos, email: str, sender: EmailSender = RealEmailSender()):
         usuario = db.Usuarios.buscar_por_email(email)
         if usuario:
             sender.enviar_mail_a(usuario, f"Recordatorio: Su clave es {usuario.obtener_clave()}")
 
-        self._responder_bien_con("Si el correo está en nuestros registros se ha enviado un recordatorio a su cuenta")
+        self._responder_bien_con(self.RECORDATORIO_EXITOSO)
