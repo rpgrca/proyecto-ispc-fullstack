4b35269d2284fa7e83047ed23d2dd59878f9b8b1
Brian Almada <brianalexisalmada84@gmail.com>
2022-10-24 22:50:37 -0300

    creacion de clase Puja

--- model/pujas.py
+++ model/pujas.py
@@ -1,4 +1,18 @@
-# pujas.py
-#
-# crear(pujador, lote, monto)
-# obtener(lote)
+from model.tipo_usuario import TipoDeUsuario
+from model.lotes import Lote
+
+
+class Puja:
+    def __init__(self, monto: int, pujador: TipoDeUsuario.pujador, lote: Lote):
+        self.__monto = monto
+        self.__pujador = pujador
+        self.__lote = lote
+
+def obtener_monto(self) -> int:
+        return self.__monto
+
+def obtener_pujador(self) -> TipoDeUsuario.pujador:
+        return self.__pujador
+
+def obtener_lote(self) -> Lote:
+        return self.__lote
