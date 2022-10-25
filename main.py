68c15d5375274089d8e0e8f59f36735984583272
Roberto <rpgrca@gmail.com>
2022-10-25 00:25:38 -0300

    usar enum en lugar de numero magico

--- main.py
+++ main.py
@@ -27,40 +27,40 @@ def __cambiar_status_code(respuesta: dict[str, str], response: Response, status_
         
     return respuesta
 
-@app.post("/ingresar/", status_code=200)
+@app.post("/ingresar/", status_code=status.HTTP_200_OK)
 def ingresar(usuario: str = Form(), clave: str = Form(), response: Response = Response()):
     controlador = LoginController(db, usuario, clave)
     return __cambiar_status_code(controlador.obtener_respuesta(), response)
 
-@app.post("/registrar/", status_code=200)
+@app.post("/registrar/", status_code=status.HTTP_200_OK)
 def registrar(nombre: str = Form(), apellido: str = Form(), email: str = Form(), usuario: str = Form(), clave: str = Form(), nacimiento: date = Form(), response: Response = Response()):
     controlador = RegistroController(db, nombre, apellido, email, usuario, clave, nacimiento)
     return __cambiar_status_code(controlador.obtener_respuesta(), response)
 
-@app.post("/reestablecer/", status_code=200)
+@app.post("/reestablecer/", status_code=status.HTTP_200_OK)
 def reestablecer(email: str = Form(), response: Response = Response()):
     controlador = ReestablecerController(db, email)
     return __cambiar_status_code(controlador.obtener_respuesta(), response)
 
-@app.post("/crear_subasta/", status_code=200)
-def crear_subasta(titulo: str = Form(), descripcion: str = Form(), imagen: str = Form(), fecha: str = Form(), response: Response = Response()):
+@app.post("/crear_subasta/", status_code=status.HTTP_200_OK)
+def crear_subasta(titulo: str = Form(), descripcion: str = Form(), imagen: str = Form(), fecha: date = Form(), response: Response = Response()):
     controlador = SubastaController(db)
     controlador.crear(titulo, descripcion, imagen, fecha)
     return __cambiar_status_code(controlador.obtener_respuesta(), response)
 
-@app.post("/agregar_lote", status_code=200)
+@app.post("/agregar_lote", status_code=status.HTTP_200_OK)
 def agregar_lote(subasta_uid: str = Form(), articulo_uid: str = Form(), base: int = Form(), response: Response = Response()):
     controlador = SubastaController(db)
     controlador.agregar_lote(subasta_uid, articulo_uid, base)
     return __cambiar_status_code(controlador.obtener_respuesta(), response)
 
-@app.get("/obtener_lote/{subasta_uid}/{orden}", status_code=200)
+@app.get("/obtener_lote/{subasta_uid}/{orden}", status_code=status.HTTP_200_OK)
 def obtener_lote(subasta_uid: str, orden: int, response: Response = Response()):
     controlador = SubastaController(db)
     controlador.obtener_lote(subasta_uid, orden)
     return __cambiar_status_code(controlador.obtener_respuesta(), response, status.HTTP_404_NOT_FOUND)
 
-@app.get("/contar_lotes/{subasta_uid}", status_code=200)
+@app.get("/contar_lotes/{subasta_uid}", status_code=status.HTTP_200_OK)
 def contar_lotes(subasta_uid: str, response: Response = Response()):
     controlador = SubastaController(db)
     controlador.contar_lotes(subasta_uid)
