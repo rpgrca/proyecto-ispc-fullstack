b3d0abfa7a9238b92696c228e6e4df8a8cd3230b
NataliaAlvarezIspc <natalia.humanista@gmail.com>
2022-10-26 20:36:46 -0300

    subo querie para traer informacion de usuario x

--- model/queries.sql
+++ model/queries.sql
@@ -1,8 +1,15 @@
 -- Lista de queries a utilizar
+
+-- Agregar un usuario
 insert into Usuarios (nombre, apellido, email, usuario, clave, nacimiento, tipo_usuario)
 values
 (%s, %s, %s, %s, %s, %s, 2);
 
+-- Agregar una subasta
 insert into Subastas (fecha, titulo, descripcion, imagen)
 values
 (%s, %s, %s, %s);
+
+-- traer información de un usuario dado
+select * from Usuarios
+%s where id = %s;
