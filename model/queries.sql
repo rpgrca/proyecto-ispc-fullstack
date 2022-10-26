5cc914d946eabca13a83b9393e3ce3208f503004
NataliaAlvarezIspc <natalia.humanista@gmail.com>
2022-10-26 18:01:33 -0300

    agrego querie con insert a subastas

--- model/queries.sql
+++ model/queries.sql
@@ -1,1 +1,8 @@
 -- Lista de queries a utilizar
+insert into Usuarios (nombre, apellido, email, usuario, clave, nacimiento, tipo_usuario)
+values
+(%s, %s, %s, %s, %s, %s, 2);
+
+insert into Subastas (fecha, titulo, descripcion, imagen)
+values
+(%s, %s, %s, %s);
