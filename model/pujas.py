8b13140813a0454ca43894101b061c600c17bc2d
Brian Almada <brianalexisalmada84@gmail.com>
2022-10-25 21:25:08 -0300

    cambio en importacion, atributos e indentacion

--- model/pujas.py
+++ model/pujas.py
@@ -1,18 +1,18 @@
-from model.tipo_usuario import TipoDeUsuario
+from model.tipo_usuario import Pujador
 from model.lotes import Lote
 
 
 class Puja:
-    def __init__(self, monto: int, pujador: TipoDeUsuario.pujador, lote: Lote):
+    def __init__(self, monto: int, pujador: Pujador, lote: Lote):
         self.__monto = monto
         self.__pujador = pujador
         self.__lote = lote
 
-def obtener_monto(self) -> int:
+    def obtener_monto(self) -> int:
         return self.__monto
 
-def obtener_pujador(self) -> TipoDeUsuario.pujador:
+    def obtener_pujador(self) -> Pujador:
         return self.__pujador
 
-def obtener_lote(self) -> Lote:
+    def obtener_lote(self) -> Lote:
         return self.__lote
