caa61f45f73cda737e317d235c0cb2f3400d707c
Brian Almada <brianalexisalmada84@gmail.com>
2022-10-26 22:00:41 -0300

    creacion de una querie para Lotes

--- model/queries.sql
+++ model/queries.sql
@@ -6,3 +6,7 @@ values
 insert into Subastas (fecha, titulo, descripcion, imagen)
 values
 (%s, %s, %s, %s);
+
+insert into Lotes (precio_base, orden, id_articulo, id_subasta)
+values
+(%s, %s, %s, %s);
