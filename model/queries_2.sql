581c883d43a500db5e1c95a74c3364379cfbe726
AhumadaCeleste <ahumadamariaceleste@gmail.com>
2022-10-29 07:52:38 -0300

    tk_77

---
+++ model/queries_2.sql
@@ -0,0 +1,62 @@
+-- Lista de queries a utilizar
+
+-- Agregar un usuario pujador
+insert into Usuarios (nombre, apellido, email, usuario, clave, nacimiento, tipo_usuario)
+values
+(%s, %s, %s, %s, %s, %s, 2);
+
+-- Agregar un usuario consignatario
+insert into Usuarios (nombre, apellido, email, usuario, clave, nacimiento, tipo_usuario)
+values
+(%s, %s, %s, %s, %s, %s, 3);
+
+-- Agregar una subasta
+insert into Subastas (fecha, titulo, descripcion, imagen)
+values
+(%s, %s, %s, %s);
+
+-- PERFIL: traer información de un usuario dado
+select * from Usuarios
+where id = %s;
+
+-- PERFIL: sobreescribir la información de un usuario dado
+UPDATE Usuarios 
+SET nombre = %s, apellido = %s, email = %s, usuario = %s, clave = %s, nacimiento = %s  
+WHERE id = %s;
+
+-- Crear un registro de Lotes
+insert into Lotes (precio_base, orden, id_articulo, id_subasta)
+values
+(%s, %s, %s, %s);
+
+-- Crear un registro de venta
+insert into Ventas (precio_final, comision, pago_consignatario, id_puja)
+values
+(%s, %s, %s, %s);
+
+-- Crear un registro de puja
+insert into Pujas (monto, id_pujador, id_lote)
+values
+(%s, %s, %s);
+
+-- Traer todas las ventas hechas para un comprador dado 
+select v.id v_id, precio_final, comision, pago_consignatario
+from Usuarios u
+inner join Pujas p on u.id = p.id_pujador
+inner join Ventas v on p.id = v.id_puja
+where u.id = %s
+
+-- Traer último registro de Subastas
+select * from Subastas
+order by Fecha  desc limit 1;
+
+-- LOGIN: Traer todos los usuarios según nombre de usuario y clave dados
+SELECT * FROM Usuarios
+WHERE usuario = %s AND clave = %s;
+
+-- Contar cantidad de uusrios con nombre
+select count(u.NOMBRE) as cantidad
+from USUARIOS u
+where u.NOMBRE is not null;
+
+
