c1caf38a2f78e0d15e04ddfe62382aba6f4c2f5d
Roberto <rpgrca@gmail.com>
2022-10-28 23:16:08 -0300

    separar servicio de consignatario del controlador

--- main_tests.py
+++ main_tests.py
@@ -4,7 +4,7 @@ from tests.puja_tests import ControladorPujaTests
 from tests.pujador_tests import ServicioPujadorTests
 from tests.consignatario_tests import ServicioConsignatarioTests
 from tests.login_tests import ServicioLoginTests
-from tests.registro_tests import ServicioRegistroTests
+from tests.registro_tests import ControladorRegistroTests
 from tests.reestablecer_tests import ServicioReestablecerTests
 from tests.usuario_tests import ControladorUsuarioTests
 from tests.subasta_tests import ControladorSubastaTests
@@ -13,7 +13,7 @@ suite = unittest.TestSuite()
 
 suite.addTest(unittest.TestLoader().loadTestsFromTestCase(ServicioLoginTests))
 suite.addTest(unittest.TestLoader().loadTestsFromTestCase(ControladorUsuarioTests))
-suite.addTest(unittest.TestLoader().loadTestsFromTestCase(ServicioRegistroTests))
+suite.addTest(unittest.TestLoader().loadTestsFromTestCase(ControladorRegistroTests))
 suite.addTest(unittest.TestLoader().loadTestsFromTestCase(ServicioReestablecerTests))
 suite.addTest(unittest.TestLoader().loadTestsFromTestCase(ServicioPujadorTests))
 suite.addTest(unittest.TestLoader().loadTestsFromTestCase(ServicioConsignatarioTests))
--- tests/registro_tests.py
+++ tests/registro_tests.py
@@ -5,15 +5,16 @@ from model.tipo_usuario import TipoDeUsuario
 from model.content_provider.memory import UsuariosEnMemoria, CreadorDeBasesDeDatosTemporales
 
 
-class ServicioRegistroTests(unittest.TestCase):
+class ControladorRegistroTests(unittest.TestCase):
     def test_graba_usuario_de_tipo_pujador(self):
         diccionario = {}
         db = CreadorDeBasesDeDatosTemporales() \
             .con_usuarios(UsuariosEnMemoria(diccionario)) \
             .construir()
 
-        ControladorRegistro(db, C.NOMBRE_USUARIO, C.APELLIDO_USUARIO, C.OTRO_EMAIL_USUARIO, C.NOMBRE_USUARIO, C.CLAVE_USUARIO,
-                         C.FECHA_NACIMIENTO_USUARIO)
+        sut = ControladorRegistro(db)
+        sut.agregar(C.NOMBRE_USUARIO, C.APELLIDO_USUARIO, C.OTRO_EMAIL_USUARIO, C.NOMBRE_USUARIO, C.CLAVE_USUARIO,
+                    C.FECHA_NACIMIENTO_USUARIO)
         self.assertEqual(TipoDeUsuario.Pujador.value, diccionario[C.NOMBRE_USUARIO]["tipo"])
 
 
